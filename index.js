22b0971b781814a7061e2547006c9812731c7636
“Matheus” <matheuscarretta@gmail.com>
2022-11-06 00:56:31 -0300

    y

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
+app.get('/cu', (req, res) => {
+  res.send('rolaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+})
+
 app.listen(process.env.PORT || 3000)
